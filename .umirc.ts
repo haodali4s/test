5eeff6431274dde23ddc17995f2b93d6c2be10ea
Liust <1294953660@qq.com>
2025-09-17 16:36:39 +0800

    feat: test

---
+++ .umirc.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  alias: {
+    "@": "/src",
+  },
+  hash: true,
+  routes: [
+    { path: "/", redirect: "/home" },
+    { path: "/home", component: "@/pages" },
+    { path: "/waterfall", component: "@/pages/waterfall" },
+    { path: "/scaling", component: "@/pages/scaling" },
+  ],
+  npmClient: "pnpm",
+});
